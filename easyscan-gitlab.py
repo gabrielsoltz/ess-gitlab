0cf08f0aa0d86839859d818f71e6f61f64c79141
gabrielsoltz <thegaby@gmail.com>
2021-09-27 15:25:00 +0200

    baseline_mode_and_dynamic_ids

--- easyscan-gitlab.py
+++ easyscan-gitlab.py
@@ -3,7 +3,6 @@
 import gitlab
 import argparse
 import json
-import pprint
 from helpers.projects.checks import *
 
 def connect(gitlab_url, gitlab_token):
@@ -30,11 +29,10 @@ def connect(gitlab_url, gitlab_token):
     # use the username/password authentication.
     #gl.auth()
 
-
 def check_project(gl, project_id):
     dict = {}
     dict.update(get_project_info(gl, project_id))
-    dict.update(check_project_visbility(gl, project_id))
+    dict.update(check_project_visibility(gl, project_id))
     dict.update(check_project_pages_access_level(gl, project_id))
     dict.update(check_project_security_and_compliance_enabled(gl, project_id))
     dict.update(check_project_approvals_before_merge(gl, project_id))
@@ -47,13 +45,32 @@ def check_project(gl, project_id):
     project_dict = {project_id: dict}
     return project_dict
 
-def check_all_projects(gl):
-    dict = {}
-    projects = get_all_projects(gl)
-    for project in projects:
-        dict_projcet = check_project(gl, project.id)
-        dict.update(dict_projcet)
-    return dict
+def check_baseline(baseline_file, scan):
+    baseline_output = {}
+    with open(baseline_file, 'r') as stream:
+        try:
+            baseline_yaml = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+        for category, ids in baseline_yaml.items():
+            baseline_id_output = {}
+            for id_dict in ids:
+                for id, values in id_dict.items():
+                    baseline_check_output = {}
+                    for value in values:
+                        for check, expected in value.items():
+                            print('Expected for ', check, 'is', expected)
+                            if expected:
+                                result = scan['easyscan-gitlab'][category][str(id)][check]
+                                if expected == result:
+                                    baseline_check_output.update({check: 'PASS'})
+                                    print ('TRUE')
+                                else:
+                                    baseline_check_output.update({check: 'FAIL'})
+                                    print('FALSE')
+            baseline_id_output.update({id: baseline_check_output})
+        baseline_output.update({category: baseline_id_output})
+    return baseline_output
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Gitlab Audit')
@@ -61,31 +78,32 @@ if __name__ == "__main__":
     parser.add_argument('--gitlab_url', help="Gitlab URL", required=True)
     parser.add_argument('--gitlab_token', help="Gitlab Token", required=True)
 
+    parser.add_argument('--mode', help="Inventory or Baseline", required=True)
+
     parser.add_argument('--check', help="Type of check (project)", required=True)
-    parser.add_argument('--project_id', help="Project ID", required=True)
+    parser.add_argument('--id', help="Gitlab Project/Group ID", required=True)
 
     args = vars(parser.parse_args())
     gitlab_url = args["gitlab_url"]
     gitlab_token = args["gitlab_token"]
-    project_id = args["project_id"]
+    mode = args["mode"]
+    id = args["id"]
     check = args["check"]
 
     gl = connect(gitlab_url, gitlab_token)
  
     if check == 'project':
-        if project_id == "all": 
-            project_output = check_all_projects(gl)
-        else:
-            project_output = check_project(gl, project_id)
-            try:
-                int(project_id)
-                project_output = check_project(gl, project_id)
-            except:
-                print ("Project Select error")
-                project_output = ""
+        projects = get_project_ids(gl, id)
+        project_output = {}
+        for project in projects:
+            dict_project = check_project(gl, project)
+            project_output.update(dict_project)
         projects_dict_output = {'projects': project_output}
-    
+
     scan = {'easyscan-gitlab': projects_dict_output}
-    
-    print(json.dumps(scan, indent=4, sort_keys=True))
-            
+    if mode == 'baseline':
+        baseline_output = {'baseline': check_baseline('baselines/default.yml', scan)}
+        output = {'easyscan-gitlab': baseline_output}
+        print(json.dumps(output, indent=4, sort_keys=True))
+    if mode == 'inventory':
+        print(json.dumps(scan, indent=4, sort_keys=True))
