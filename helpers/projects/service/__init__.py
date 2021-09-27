b2b9d708695c1f709ba0af3099a59f95e714d177
gabrielsoltz <thegaby@gmail.com>
2021-09-27 15:25:50 +0200

    rename_to_service

--- helpers/projects/service/__init__.py
+++ helpers/projects/service/__init__.py
@@ -1,5 +1,3 @@
-import yaml
-
 def get_protectedbranches(gl, project_id):
     project = gl.projects.get(project_id)
     return project.protectedbranches.list()
@@ -32,10 +30,38 @@ def get_project_contributors(gl, project_id):
     project = gl.projects.get(project_id)
     return project.repository_contributors()
 
-def get_all_projects(gl):
-    #projects = gl.projects.list(all=True)
-    projects = gl.groups.list(page=1, per_page=10)
-    return projects
+def get_project_ids(gl, id):
+
+    project_ids_list = []
+
+    if id == 'all':
+        print ("it's a all")
+        #projects = gl.projects.list(all=True)
+        projects = gl.groups.list(page=1, per_page=10, lazy=True)
+        for project in projects:
+            project_ids_list.append(project.id)
+        return project_ids_list
+
+    try:
+        group = gl.groups.get(id, lazy=True)
+        projects = group.projects.list(include_subgroups=True, all=True, lazy=True)
+    #except gitlab.exceptions.GitlabListError:
+    except:
+        group = None
+    if group:
+        print ("it's a group")
+        for project in projects:
+            project_ids_list.append(project.id)
+        return project_ids_list
+
+    try:
+        project = gl.projects.get(id)
+    except:
+        project = None
+    if project:
+        print ("it's a project")
+        project_ids_list.append(id)
+        return project_ids_list
 
 def get_project_file(gl, project_id, file):
     project = gl.projects.get(project_id)
