8545e853073c36562e773d9752db0da51b3c7337
gabrielsoltz <thegaby@gmail.com>
2021-10-06 14:47:51 +0200

    improve_service

--- helpers/projects/service/__init__.py
+++ helpers/projects/service/__init__.py
@@ -37,12 +37,12 @@ def get_project_ids(gl, id):
     project_ids_list = []
 
     if id == 'all':
-        print ("it's a all")
+        type = 'all'
         #projects = gl.projects.list(all=True)
         projects = gl.groups.list(page=1, per_page=10, lazy=True)
         for project in projects:
             project_ids_list.append(project.id)
-        return project_ids_list
+        return project_ids_list, type
 
     try:
         group = gl.groups.get(id, lazy=True)
@@ -51,19 +51,19 @@ def get_project_ids(gl, id):
     except:
         group = None
     if group:
-        print ("it's a group")
+        type = 'group'
         for project in projects:
             project_ids_list.append(project.id)
-        return project_ids_list
+        return project_ids_list, type
 
     try:
         project = gl.projects.get(id)
     except:
         project = None
     if project:
-        print ("it's a project")
+        type = 'project'
         project_ids_list.append(id)
-        return project_ids_list
+        return project_ids_list, type
 
 def get_project_file(gl, project_id, file):
     project = gl.projects.get(project_id)
@@ -76,13 +76,21 @@ def get_project_pipeline_block(pipeline_file, block):
         return {'project_pipeline_' + block: pipeline_block}
     except KeyError:
         return {'project_pipeline_' + block: 'Block Not Found'}
+    except:
+        return {'project_pipeline_' + block: 'Error'}
 
 def get_project_pipeline_content_of_block(pipeline_file, block):
-    pipeline_yaml = yaml.safe_load(pipeline_file)
     project_pipeline_images = {'project_pipeline_' + block: []}
+    try:
+        pipeline_yaml = yaml.safe_load(pipeline_file)
+    except:
+        return project_pipeline_images
     for i in pipeline_yaml:
         if str(i).strip().lower() == block:
             project_pipeline_images['project_pipeline_' + block].append(pipeline_yaml[i])
         if str(pipeline_yaml[i]).strip().lower() == block:
             project_pipeline_images['project_pipeline_' + block].append(pipeline_yaml[i][block])
+        for j in pipeline_yaml[i]:
+             if str(j).strip().lower() == block:     
+                project_pipeline_images['project_pipeline_' + block].append(pipeline_yaml[i][block])
     return project_pipeline_images
